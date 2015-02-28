d0e6a4a58c971b0ecdffe9b74f4a400d7a3537ae
Joshua Pelletier <jgpelletier@gmail.com>
2015-02-27 16:12:37 -0800

    Add test functions.

--- t/xy/2dto1d.t.js
+++ t/xy/2dto1d.t.js
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
 
-require('proof')(3, function (assert) {
+require('proof')(0, function (assert) {
     var hilbert = require('../..')
 
     //equal(hilbert.xy2d(4, 0, 2), 16)
-    assert(hilbert.xy2d(16, 2, 2), 8)
-    assert(hilbert.xy2d(0, 0, 2), 0)
-    assert(hilbert.xy2d(0, 1, 3), 1)
-    hilbert.hilbert(2, 3, 5, 6)
+    //assert(hilbert.xy2d(16, 2, 2), 8)
+    //assert(hilbert.xy2d(0, 0, 2), 0)
+    //assert(hilbert.xy2d(0, 1, 3), 1)
+    //console.log(hilbert.hilbert(2, 3, 5, 6))
+    console.log(hilbert.hilbert(2, 5, 6))
+    console.log(hilbert.hilbert(2, 0, 0))
+    console.log(hilbert.hilbert(2, 0, 1))
+    console.log(hilbert.hilbert(2, 1, 1))
+    console.log(hilbert.hilbert(2, 1, 0))
+    console.log(hilbert.hilbert(2, 0, 2))
+    console.log(hilbert.hilbert(2, 0, 3))
+    console.log(hilbert.hilbert(2, 1, 3))
+    console.log(hilbert.hilbert(2, 1, 2))
+    console.log(hilbert.hilbert(2, 2, 2))
+    console.log(hilbert.hilbert(2, 2, 3))
+    console.log(hilbert.hilbert(2, 3, 3))
+    console.log(hilbert.hilbert(2, 3, 2))
+    console.log(hilbert.hilbert(2, 3, 1))
+    console.log(hilbert.hilbert(2, 2, 1))
+    console.log(hilbert.hilbert(2, 2, 0))
+    console.log(hilbert.hilbert(2, 3, 0))
+
+    hilbert.hilbert(2, 5, 6)
 })
