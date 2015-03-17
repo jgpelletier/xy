142a6d20cf888b4068ddd50a38490c38d2023e79
Joshua Pelletier <jgpelletier@gmail.com>
2015-03-17 12:58:34 -0700

    Add test for high index in 2d.

--- t/xy/1dto2d.t.js
+++ t/xy/1dto2d.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('proof')(38, prove)
+require('proof')(39, prove)
 
 function prove (assert) {
     var hilbert = require('../..')
@@ -13,6 +13,7 @@ function prove (assert) {
     assert(hilbert.hilbertInverse(2,6), [1,3], "index 6 equals point [1,3]")
     assert(hilbert.hilbertInverse(2,7), [1,2], "index 7 equals point [1,2]")
     assert(hilbert.hilbertInverse(2,8), [2,2], "index 8 equals point [2,2]")
+    assert(hilbert.hilbertInverse(2, 1073741823), [32767,0], "index 1073741823 equals point [32767,0]")
 
     // works when precision is 2
     assert(hilbert.hilbertInverse(3,0), [0,0,0], "index 0 equals point [0,0,0]")
